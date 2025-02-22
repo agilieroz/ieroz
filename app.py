cf700c139a72cca15cad024086d37449b4b92197
agilieroz <ieroz.web.id@gmail.com>
2025-02-22 09:01:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -129,5 +129,5 @@ def social():
 def home():
     return render_template('home.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
